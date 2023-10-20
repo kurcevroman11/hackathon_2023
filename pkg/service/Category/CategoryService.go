c00b4aa7eda23b4aaded9b8157ea2fb878d16f32
roman.kurtsev <roman.kurtsev@sebbia.org>
2023-10-21 01:05:09 +0300

    Added GenerateUUID

--- pkg/service/Category/CategoryService.go
+++ pkg/service/Category/CategoryService.go
@@ -3,16 +3,17 @@ package Category
 import (
 	"github.com/zhashkevych/todo-app/pkg/models"
 	"github.com/zhashkevych/todo-app/pkg/repository"
+	"github.com/zhashkevych/todo-app/pkg/tools"
 	"gorm.io/gorm/logger"
 )
 
 type CategoryService struct {
 	rep    *repository.Repository
-	gen    *string
+	gen    *tools.UUIDStringGenerator
 	Logger logger.Interface
 }
 
-func NewCategoryService(rep *repository.Repository, gen *string, Logger logger.Interface) CategoryService {
+func NewCategoryService(rep *repository.Repository, gen *tools.UUIDStringGenerator, Logger logger.Interface) CategoryService {
 	return CategoryService{
 		rep:    rep,
 		gen:    gen,
